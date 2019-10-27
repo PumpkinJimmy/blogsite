6e245427963b427a9952530af904b3698a5c4087
bbpumpkin <996344439@qq.com>
2019-10-27 21:20:03 +0800

    Build model Article, build several template html, both not test yet

--- myblog/urls.py
+++ myblog/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
 from . import views
 urlpatterns = [
-    url(r'^$', views.indexView)
-
+    url(r'^$', views.indexView),
+    url(r'^article/$', views.articleListView),
+    url(r'^article/(?P<id>[0-9]+)$', views.articleView, name="article")
 ]
--- myblog/views.py
+++ myblog/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
+from .models import Article
 # Create your views here.
 
 
 def indexView(request):
     return render(request, 'myblog/index.html')
+
+def articleView(request, pk):
+    art = Article.objects.get(pk)
+
+    return render(request, 'myblog/article.html', context={'article': art})
+
+def articleListView(request):
+    arts = Article.objects.all()
+    return render(request, 'myblog/article_list.html',
+                  context={'articles':arts})
+
