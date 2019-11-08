a316bec6a1a420eabdfe03eb8411abbd4dfb88af
bbpumpkin <996344439@qq.com>
2019-11-08 22:11:13 +0800

    Prepare a effect.css, build a prototype upload page

--- myblog/urls.py
+++ myblog/urls.py
@@ -3,5 +3,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.indexView, name="index"),
     url(r'^article/$', views.articleListView, name="article_list"),
-    url(r'^article/(?P<id>[0-9]+)$', views.articleView, name="article")
+    url(r'^article/(?P<id>[0-9]+)$', views.articleView, name="article"),
+    url(r'^upload/$', views.uploadView)
 ]
--- myblog/views.py
+++ myblog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Article
 from md_parse.parse import Parser, HtmlRenderer
+from django.http.response import HttpResponse
 # Create your views here.
 
 
@@ -19,4 +20,14 @@ def articleListView(request):
     arts = Article.objects.all()
     return render(request, 'myblog/article_list.html',
                   context={'articles':arts})
+
+def uploadView(request):
+    if request.method == "GET":
+        return render(request, 'myblog/upload.html')
+    else:
+        data = str(request.FILES['f'].read(), encoding="utf-8")
+        art = Article(title="Upload Article", content=data, brief="")
+        art.save()
+        return HttpResponse("Success")
+
 
