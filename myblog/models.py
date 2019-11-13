cfe23f9629aee50efda64a7ab953c0e0f6f98672
bbpumpkin <996344439@qq.com>
2019-11-13 18:39:30 +0800

    add Tag model, add date and tag features to Aritcle model

--- myblog/models.py
+++ myblog/models.py
@@ -3,9 +3,17 @@ from django.db import models
 # Create your models here.
 
 
+class Tag(models.Model):
+    name = models.CharField(max_length=30)
+    father = models.ForeignKey('self', models.CASCADE)
+
+
 class Article(models.Model):
     title = models.CharField(max_length=200)
     brief = models.CharField(max_length=400)
     content = models.TextField()
+    date = models.DateField(auto_now_add=True)
+    tags = models.ManyToManyField('Tag')
+
     def __str__(self):
         return self.title
