c10decf913c97f4a8de1b924098df0d88d0d9591
bbpumpkin <996344439@qq.com>
2020-02-29 23:58:22 +0800

    将之前设计好的外观放到了Django项目里面

---
+++ TODO.md
@@ -0,0 +1,5 @@
+## TODO List
+1. fix the li bug of abnormal formal
+2. quote the content (like gt, lt)
+3. fix the ol unmatch bug
+4. 尝试实现完整的Parser
--- myblog/models.py
+++ myblog/models.py
@@ -5,7 +5,9 @@ from django.db import models
 
 class Tag(models.Model):
     name = models.CharField(max_length=30)
-    father = models.ForeignKey('self', models.CASCADE)
+    father = models.ForeignKey('self', models.CASCADE, blank=True, null=True)
+    def __str__(self):
+        return self.name
 
 
 class Article(models.Model):
@@ -13,7 +15,7 @@ class Article(models.Model):
     brief = models.CharField(max_length=400)
     content = models.TextField()
     date = models.DateField(auto_now_add=True)
-    tags = models.ManyToManyField('Tag')
+    tags = models.ManyToManyField('Tag', blank=True)
 
     def __str__(self):
         return self.title
